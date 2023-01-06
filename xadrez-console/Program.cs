3a0e17fbfb2f230217c9ff4c2263f4770e24a44c
Lucas Bardelli <lucas.bardelli@hotmail.com>
2023-01-06 15:58:16 -0300

    Criando a classe PosicaoXadrez

--- xadrez-console/Program.cs
+++ xadrez-console/Program.cs
@@ -3,22 +3,30 @@ using tabuleiro;
 using xadrez;
 using xadrez_console.tabuleiro;
 
-namespace xadrez_console {
-    internal class Program {
-        static void Main(string[] args) {
-            try {
+namespace xadrez_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                PosicaoXadrez pos = new PosicaoXadrez('a', 1);
+                Console.WriteLine(pos);
 
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                Console.WriteLine(pos.toPosicao());
+                /*Tabuleiro tab = new Tabuleiro(8, 8);
 
                 tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
                 tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
 
-                Tela.imprimirTabuleiro(tab);
+                Tela.imprimirTabuleiro(tab);*/
             }
 
-            
-            catch (TabuleiroException e) {
+
+            catch (TabuleiroException e)
+            {
                 Console.WriteLine(e.Message);
             }
 
--- xadrez-console/tabuleiro/Posicao.cs
+++ xadrez-console/tabuleiro/Posicao.cs
@@ -1,19 +1,24 @@
 ﻿using System;
 
-namespace tabuleiro {
-    internal class Posicao {
-
+namespace tabuleiro
+{
+    public class Posicao
+    {
         public int linha { get; set; }
         public int coluna { get; set; }
 
-        public Posicao() {
+        public Posicao()
+        {
         }
-        public Posicao(int linha, int coluna) {
+
+        public Posicao(int linha, int coluna)
+        {
             this.linha = linha;
             this.coluna = coluna;
         }
 
-        override public string ToString() {
+        public override string ToString()
+        {
             return linha + ", " + coluna;
         }
     }
